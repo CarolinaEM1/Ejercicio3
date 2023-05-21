748a2c51b91ef0bd855898fe9bb53933644231e6
Carolina Martinez <carolinamtz091@gmail.com>
2023-05-21 10:54:43 -0600

    primera version

---
+++ Ejercicio3.py
@@ -0,0 +1,11 @@
+#Intercambiar el valor de 2 valores
+a = input("a ->")
+b = input("b ->")
+
+a,b = b,a
+print(f"El nuevo valor de a es: {a}")
+print(f"El nuevo valor de b es: {b}")
+
+#Carolina EM
+
+
